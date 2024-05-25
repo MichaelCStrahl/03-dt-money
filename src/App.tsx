2b0d50fe8dcb94bd6d71bce86f6154f213e0aa37
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-05-25 12:47:01 -0300

    add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { TransactionsProvider } from "./contexts/TransactionsContext";
-import { Transactions } from "./pages/Transactions/inex";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
